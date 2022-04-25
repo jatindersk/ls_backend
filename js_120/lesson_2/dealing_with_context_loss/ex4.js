5c44395e50446465e55cfdc911144d8fbc37852c
jatindersk <jatinderskk8@gmail.com>
2022-04-26 00:54:26 +0530

    js120

--- js_120/lesson_2/dealing_with_context_loss/ex4.js
+++ js_120/lesson_2/dealing_with_context_loss/ex4.js
@@ -8,4 +8,11 @@ const TESgames = {
   }
 };
 
-TESgames.listGames();
+TESgames.listGames();     //  undefined + ': ' + title
+
+//  It will not produce the desired output.
+//  This is because the function argument in forEach method
+//  looses it's context and thus `this` will point to the
+//  global object. Since the global object doesn't have the
+//  property named `seriesTitle`, it will return undefined for
+//  this.seriesTitle expression.
