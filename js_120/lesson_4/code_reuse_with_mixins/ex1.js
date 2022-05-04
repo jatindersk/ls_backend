f0ed9c05c3b411f33d95edb7621ba31992298808
jatindersk <jatinderskk8@gmail.com>
2022-05-04 23:56:20 +0530

    js120

--- js_120/lesson_4/code_reuse_with_mixins/ex1.js
+++ js_120/lesson_4/code_reuse_with_mixins/ex1.js
@@ -1,1 +1,36 @@
-console.log('hello');
+const Speed = {
+  goFast() {
+    console.log(`I'm a ${this.constructor.name} and going super fast!`);
+  }
+};
+
+class Car {
+  goSlow() {
+    console.log(`I'm safe and driving slow.`);
+  }
+}
+
+Object.assign(Car.prototype, Speed);
+
+class Truck {
+  goVerySlow() {
+    console.log(`I'm a heavy truck and like going very slow.`);
+  }
+}
+
+Object.assign(Truck.prototype, Speed);
+
+
+let blueTruck = new Truck();
+blueTruck.goFast(); // => logs "I'm a Truck and going super fast!"
+
+let smallCar = new Car();
+smallCar.goFast(); // => logs "I'm a Car and going super fast!"
+
+'goFast' in smallCar;  // => true
+'goFast' in blueTruck; // => true
+
+
+//  Since the constructor property references a function object,
+//  constructor.name references the name property on that object.
+//  Use MDN to lookup the definition of Function.name.
