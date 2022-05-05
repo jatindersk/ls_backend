0c483e63fa4d1473ff10eb3bc4830987edde2c27
jatindersk <jatinderskk8@gmail.com>
2022-05-06 05:12:41 +0530

    js120

---
+++ js_120/rough_work/ex1.js
@@ -0,0 +1,66 @@
+//  Context loss
+//  1. Method Copying
+
+// function repeatSelf(func) {
+//   func();
+//   func();
+// }
+
+// function test() {
+//   let person = {
+//     name: 'jatinder',
+//     age: 30,
+//     introduce: function () {
+//       console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
+//     },
+//   };
+
+//   repeatSelf(person.introduce);
+// }
+
+// test();
+
+//  Solution 1:
+//  supply the execution context as the second parameter to
+//  repeatSelf function
+
+// function repeatSelf(func, context) {
+//   func.call(context);
+//   func.call(context);
+// }
+
+// function test() {
+//   let person = {
+//     name: 'jatinder',
+//     age: 30,
+//     introduce: function () {
+//       console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
+//     },
+//   };
+
+//   repeatSelf(person.introduce, person);
+// }
+
+// test();
+
+//  Solution 2:
+//  use the bind method
+
+function repeatSelf(func, context) {
+  func.call(context);
+  func.call(context);
+}
+
+function test() {
+  let person = {
+    name: 'jatinder',
+    age: 30,
+    introduce: function () {
+      console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
+    },
+  };
+
+  repeatSelf(person.introduce.bind(person));
+}
+
+test();
