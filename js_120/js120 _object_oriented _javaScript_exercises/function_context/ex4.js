a168cfbb465294b59c3d36cceb9a34a369d7195f
jatindersk <jatinderskk8@gmail.com>
2022-05-16 08:22:32 +0530

    js120

---
+++ js_120/js120 _object_oriented _javaScript_exercises/function_context/ex4.js
@@ -0,0 +1,26 @@
+function myFilter(array, func, thisArg = this) {
+  let result = [];
+
+  array.forEach(function(value) {
+    if (func.call(thisArg, value)) {
+      result.push(value);
+    }
+  });
+
+  return result;
+}
+
+let filter = {
+  allowedValues: [5, 6, 9],
+};
+
+let result = myFilter([2, 1, 3, 4, 5, 6, 9, 12], function(val) {
+  return this.allowedValues.indexOf(val) >= 0;
+}, filter); // returns [5, 6, 9]
+
+// let result2 = myFilter([2, 1, 3, 4, 5, 6, 9, 12], function(val) {
+//   return this.allowedValues.indexOf(val) >= 0;
+// });
+
+console.log(result);
+// console.log(result2);
