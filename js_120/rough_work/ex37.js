f6dcea9e4342ac4efabfe715dcf345216be558b9
jatindersk <jatinderskk8@gmail.com>
2022-06-05 23:51:46 +0530

    js120

---
+++ js_120/rough_work/ex37.js
@@ -0,0 +1,17 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  speaks() {
+    return `${this.name} says meowwww.`;
+  }
+}
+
+let fakeCat = Object.create(Cat.prototype); // your implementation
+console.log(fakeCat);
+console.log(fakeCat instanceof Cat); // logs true
+console.log(fakeCat.name);           // logs undefined
+console.log(fakeCat.speaks());       // logs undefined says meowwww.
+
+let fakeCat1 = new Cat(); // your implementation
+console.log(fakeCat1);
