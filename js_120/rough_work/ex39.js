4485f9b5ae4718fdf8838148d826efec8febec02
jatindersk <jatinderskk8@gmail.com>
2022-06-08 09:44:05 +0530

    js120

---
+++ js_120/rough_work/ex39.js
@@ -0,0 +1,20 @@
+class Pet {
+  constructor(name, age, color = undefined) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+  }
+}
+
+class Cat extends Pet {
+
+  info() {
+    return `My cat ${this.name} is ${this.age} years old and has ${this.color} fur.`;
+  }
+}
+
+let pudding = new Cat('Pudding', 7, 'black and white');
+let butterscotch = new Cat('Butterscotch', 10, 'tan and white');
+
+console.log(pudding.info());
+console.log(butterscotch.info());
