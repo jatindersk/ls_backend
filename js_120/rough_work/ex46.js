c139597b7aeff6ff20bf3afa7eb44183bc904c11
jatindersk <jatinderskk8@gmail.com>
2022-06-10 22:26:34 +0530

    js120

--- js_120/rough_work/ex46.js
+++ js_120/rough_work/ex46.js
@@ -1,17 +1,38 @@
 class Banner {
   constructor(message) {
-
+    this.message = message;
   }
 
   displayBanner() {
     console.log([this.horizontalRule(), this.emptyLine(), this.messageLine(), this.emptyLine(), this.horizontalRule()].join("\n"));
   }
 
-  horizontalRule() { }
+  horizontalRule() {
+    return `+-${'-'.repeat(this.message.length) }-+`;
+  }
 
-  emptyLine() { }
+  emptyLine() {
+    return `| ${' '.repeat(this.message.length) } |`;
+  }
 
   messageLine() {
     return `| ${this.message} |`;
   }
 }
+
+let banner1 = new Banner('To boldly go where no one has gone before.');
+banner1.displayBanner();
+// +--------------------------------------------+
+// |                                            |
+// | To boldly go where no one has gone before. |
+// |                                            |
+// +--------------------------------------------+
+
+
+let banner2 = new Banner('');
+banner2.displayBanner();
+// +--+
+// |  |
+// |  |
+// |  |
+// +--+
