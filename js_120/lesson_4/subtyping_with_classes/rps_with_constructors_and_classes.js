aa2bb11a0bd3edc94c17cc346dcfbac68450cf1a
jatindersk <jatinderskk8@gmail.com>
2022-06-30 02:42:29 +0530

    js120

---
+++ js_120/lesson_4/subtyping_with_classes/rps_with_constructors_and_classes.js
@@ -0,0 +1,91 @@
+let readline = require('readline-sync');
+
+function createPlayer() {
+  this.move = null;
+}
+
+function createComputer() {
+  this.playerObject = new createPlayer();
+  this.computerObject = {
+    choose() {
+      const choices = ['rock', 'paper', 'scissors'];
+      let randomIndex = Math.floor(Math.random() * choices.length);
+      this.move = choices[randomIndex];
+    },
+  };
+
+  return Object.assign(this.playerObject, this.computerObject);
+}
+
+function createHuman() {
+  this.playerObject = new createPlayer();
+  this.humanObject = {
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log('Please choose rock, paper, or scissors:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        console.log('Sorry, invalid choice.');
+      }
+
+      this.move = choice;
+    },
+  };
+
+  return Object.assign(this.playerObject, this.humanObject);
+}
+
+const RPSGame = {
+  human: createHuman(),
+  computer: createComputer(),
+
+  displayWelcomeMessage() {
+    console.log('Welcome to Rock, Paper, Scissors!');
+  },
+
+  displayGoodbyeMessage() {
+    console.log('Thanks for playing Rock, Paper, Scissors. Goodbye!');
+  },
+
+  displayWinner() {
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+
+    if ((humanMove === 'rock' && computerMove === 'scissors') ||
+        (humanMove === 'paper' && computerMove === 'rock') ||
+        (humanMove === 'scissors' && computerMove === 'paper')) {
+      console.log('You win!');
+    } else if ((humanMove === 'rock' && computerMove === 'paper') ||
+               (humanMove === 'paper' && computerMove === 'scissors') ||
+               (humanMove === 'scissors' && computerMove === 'rock')) {
+      console.log('Computer wins!');
+    } else {
+      console.log("It's a tie");
+    }
+  },
+
+  playAgain() {
+    console.log('Would you like to play again? (y/n)');
+    let answer = readline.question();
+    return answer.toLowerCase()[0] === 'y';
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      if (!this.playAgain()) break;
+    }
+
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSGame.play();
---
+++ js_120/rough_work/ex90.js
@@ -0,0 +1,17 @@
+const Speed = {
+  goFast() {
+    console.log(`I'm a ${this.constructor.name} and going super fast!`);
+  }
+};
+
+class Car {
+  goSlow() {
+    console.log(`I'm safe and driving slow.`);
+  }
+}
+
+class Truck {
+  goVerySlow() {
+    console.log(`I'm a heavy truck and like going very slow.`);
+  }
+}
