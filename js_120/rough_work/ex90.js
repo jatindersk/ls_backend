d3d3fb16214849b8bed70ce04b5789e3885c9c5b
jatindersk <jatinderskk8@gmail.com>
2022-07-02 21:43:42 +0530

    js120

--- js_120/rough_work/ex90.js
+++ js_120/rough_work/ex90.js
@@ -10,8 +10,26 @@ class Car {
   }
 }
 
+Object.assign(Car.prototype, Speed);
+
 class Truck {
   goVerySlow() {
     console.log(`I'm a heavy truck and like going very slow.`);
   }
 }
+
+Object.assign(Truck.prototype, Speed);
+
+let car = new Car();
+console.log('goFast' in car);
+car.goFast();
+
+let truck = new Truck();
+console.log('goFast' in truck);
+truck.goFast();
+
+//  The prototype object of the instance
+//  (which is also the prototype object of the constructor)
+//  has a .constructor property. This property further has a
+//  'name' property which simply contains the name of the
+//  constructor as a string.
