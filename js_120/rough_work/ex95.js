93616f572f470a137660980159f40050d66c344e
jatindersk <jatinderskk8@gmail.com>
2022-07-08 22:14:12 +0530

    js120

--- js_120/rough_work/ex95.js
+++ js_120/rough_work/ex95.js
@@ -16,3 +16,12 @@ class Rectangle {
     return this.width * this.length;
   }
 }
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+}
+
+let square = new Square(5);
+console.log(`area of square = ${square.getArea()}`); // area of square = 25
