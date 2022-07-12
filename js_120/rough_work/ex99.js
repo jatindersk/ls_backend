e90708300b0c732bef50b22c74973265860ef652
jatindersk <jatinderskk8@gmail.com>
2022-07-12 23:54:25 +0530

    js120

---
+++ js_120/rough_work/ex99.js
@@ -0,0 +1,45 @@
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getWheels() {
+    return 4;
+  }
+
+  info() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+class Motorcycle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getWheels() {
+    return 2;
+  }
+
+  info() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+class Truck {
+  constructor(make, model, payload) {
+    this.make = make;
+    this.model = model;
+    this.payload = payload;
+  }
+
+  getWheels() {
+    return 6;
+  }
+
+  info() {
+    return `${this.make} ${this.model}`;
+  }
+}
