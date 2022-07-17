478668f38f48ef104efe28459f7f955d304cbf59
jatindersk <jatinderskk8@gmail.com>
2022-07-17 11:31:11 +0530

    js120

--- js_120/rough_work/ex109.js
+++ js_120/rough_work/ex109.js
@@ -1,7 +1,21 @@
-let person = {
-  firstName: 'Rick ',
-  lastName: 'Sanchez',
-  fullName: this.firstName + this.lastName,
+function myFilter(array, func, filter) {
+  let result = [];
+
+  array.forEach(function(value) {
+    if (func.bind(filter, value)()) {
+      result.push(value);
+    }
+  });
+
+  return result;
+}
+
+let filter = {
+  allowedValues: [5, 6, 9],
 };
 
-console.log(person.fullName);
+let result = myFilter([2, 1, 3, 4, 5, 6, 9, 12], function(val) {
+  return this.allowedValues.indexOf(val) >= 0;
+}, filter); // returns [5, 6, 9]
+
+console.log(result);
