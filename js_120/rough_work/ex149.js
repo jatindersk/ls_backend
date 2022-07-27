048be12a8edb4bb84a95faa3414ad783e847f6f8
jatindersk <jatinderskk8@gmail.com>
2022-07-27 22:59:36 +0530

    js120

---
+++ js_120/rough_work/ex149.js
@@ -0,0 +1,20 @@
+function objectsEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key in obj1) {
+    if (!obj2.hasOwnProperty(key)) return false;
+    else if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+}
+
+console.log(objectsEqual({a: 'foo'}, {a: 'foo'}));                      // true
+console.log(objectsEqual({a: 'foo', b: 'bar'}, {a: 'foo'}));            // false
+console.log(objectsEqual({}, {}));                                      // true
+console.log(objectsEqual({a: 'foo', b: undefined}, {a: 'foo', c: 1}));  // false
